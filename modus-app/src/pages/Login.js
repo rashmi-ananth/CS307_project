c85c24ab3ef689d1e9c6e222765b4c4e1ef839cf
JungminKim1101 <54600334+JungminKim1@users.noreply.github.com>
2021-11-05 06:41:22 -0400

    Added Login CSS
    LOgin CSS

--- modus-app/src/pages/Login.js
+++ modus-app/src/pages/Login.js
@@ -25,7 +25,7 @@ function Login() {
 
   return (
     <div className="loginPage">
-      <h2>Log in </h2>
+      {/* <h2>Log in </h2> */}
       <div className="loginContainer">
         <input
           type="text"
