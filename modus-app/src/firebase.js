b87b4f0b834b7b4b3ce33594e6846a9965308ef0
bhatia47 <bhatia47@purdue.edu>
2021-11-21 22:08:00 -0500

    add capability to transfer files between buckets on firestore

--- modus-app/src/firebase.js
+++ modus-app/src/firebase.js
@@ -524,3 +524,43 @@ function plotPieChart(dict_t2e) {
             });
     }
 }
+
+function moveFirestoreDocument(DocumentReference fromPath, final DocumentReference toPath) {
+  fromPath.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+  onComplete(@NonNull Task<DocumentSnapshot> task) {
+          if (task.isSuccessful()) {
+              DocumentSnapshot document = task.getResult();
+              if (document != null) {
+                  toPath.set(document.getData())
+                      .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    
+                          onSuccess(Void aVoid) {
+                              Log.d(TAG, "DocumentSnapshot successfully written!");
+                              fromPath.delete()
+                              .addOnSuccessListener(new OnSuccessListener<Void>() {
+        
+                                      onSuccess(Void aVoid) {
+                                          Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                                      }
+                              })
+                              .addOnFailureListener(new OnFailureListener() {
+                                      onFailure(@NonNull Exception e) {
+                                          Log.w(TAG, "Error deleting document", e);
+                                      }
+                              });
+                          }
+                      })
+                      .addOnFailureListener(new OnFailureListener() {
+                         onFailure(@NonNull Exception e) {
+                              Log.w(TAG, "Error writing document", e);
+                          }
+                      });
+              } else {
+                  Log.d(TAG, "No such document");
+              }
+          } else {
+              Log.d(TAG, "get failed with ", task.getException());
+          }
+      }
+  });
+}
