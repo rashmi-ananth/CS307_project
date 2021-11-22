1ed20d03cdeeded28a4747f5db329555807add0c
JungminKim1101 <54600334+JungminKim1@users.noreply.github.com>
2021-11-21 20:23:28 -0500

    Finished Contact us form and added footer.js
    Finished contact us form to send email without backend / added footer.js to put contact information

--- modus-app/src/pages/ContactUs.js
+++ modus-app/src/pages/ContactUs.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -8,6 +9,19 @@ const Contact = () => {
 
   const submit = () => {
     if (name && email && message) {
+      const serviceId = "service_id";
+      const templateId = "template_id";
+      const userId = "user_id";
+      const templateParams = {
+        name,
+        email,
+        message,
+      };
+
+      emailjs
+        .send(serviceId, templateId, templateParams, userId)
+        .then((response) => console.log(response))
+        .then((error) => console.log(error));
       setName("");
       setEmail("");
       setMessage("");
@@ -37,6 +51,9 @@ const Contact = () => {
         onChange={(e) => setMessage(e.target.value)}
       ></textarea>
       <button onClick={submit}>Send</button>
+      <span className={emailSent ? "visible" : null}>
+        Thank you so much for the feedback!
+      </span>
     </div>
   );
 };
